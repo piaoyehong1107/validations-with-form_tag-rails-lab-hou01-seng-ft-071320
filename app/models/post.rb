b7f485b8c0a44868e572eb319b8084120b943df1
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-20 16:11:16 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true 
-  validates :category inclusion: {in: %w(Finction Non-Fiction)}
+  validates :category, inclusion: {in: %w(Finction Non-Fiction)}
   validates :content, length: {minimum: 100}
 end
